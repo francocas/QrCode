67763b394bccab8e9574d2e5c6388acdfa66d5f1
francocas <franquicas@hotmail.com>
2017-09-09 01:58:09 -0300

    a

---
+++ src/pages/klghufklgj/klghufklgj.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { KlghufklgjPage } from './klghufklgj';
+
+@NgModule({
+  declarations: [
+    KlghufklgjPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(KlghufklgjPage),
+  ],
+})
+export class KlghufklgjPageModule {}
---
+++ src/pages/klghufklgj/klghufklgj.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the KlghufklgjPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-klghufklgj',
+  templateUrl: 'klghufklgj.html',
+})
+export class KlghufklgjPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad KlghufklgjPage');
+  }
+
+}
